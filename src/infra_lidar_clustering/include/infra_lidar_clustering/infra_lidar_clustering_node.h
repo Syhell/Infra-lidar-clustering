1256a41fb6fb27f2412e9cbc85cde088a86ca6b6
Syhell <bq18095@shibaura-it.ac.jp>
2023-08-08 17:14:18 +0900

    update_ver0.1

--- src/infra_lidar_clustering/include/infra_lidar_clustering/infra_lidar_clustering_node.h
+++ src/infra_lidar_clustering/include/infra_lidar_clustering/infra_lidar_clustering_node.h
@@ -6,6 +6,15 @@
 #include <visualization_msgs/MarkerArray.h>
 #include <visualization_msgs/Marker.h>
 
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include "itolab_senior_car_msgs/Centroids.h"
+#include "itolab_senior_car_msgs/CloudClusterArray.h"
+#include "itolab_senior_car_msgs/DetectedObjectArray.h"
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -18,12 +27,25 @@ namespace infra_lidar_clustering_node
     {
         public:
             LidarClusteringNode();
+            void sync_callback(const geometry_msgs::PoseStamped::ConstPtr& in_pose_msg,
+                               const sensor_msgs::PointCloud2ConstPtr& in_cloud_msg);
+            void clustering(const pcl::PointCloud<pcl::PointXYZ>::Ptr in_ptr, 
+                            std::vector<lidar_clustering::ClusterPtr>& clusters,
+                            itolab_senior_car_msgs::Centroids& centroids,
+                            float max_cluster_distance); 
         private:
-        ros::NodeHandle nh;
-        ros::Publisher cluster_cloud_pub;
-        ros::Subscriber point_cloud_sub;
+            ros::NodeHandle nh;
+            
+            ros::Publisher cluster_cloud_pub;
+            
+            message_filters::Subscriber<geometry_msgs::PoseStamped> pose_sub;
+            message_filters::Subscriber<sensor_msgs::PointCloud2> clippedCloud_sub;
+            typedef message_filters::sync_policies::ApproximateTime<geometry_msgs::PoseStamped, sensor_msgs::PointCloud2> SyPl;
+            message_filters::Synchronizer<SyPl> Sync;
 
-        std::string sub_topic = "cliped_cloud";
+            double clip_min_height = -0.85;
+            double clip_max_height = 0.5;
+            std::string sub_topic = "cliped_cloud";
         
         
 
--- src/infra_lidar_clustering/src/infra_lidar_clustering_node.cpp
+++ src/infra_lidar_clustering/src/infra_lidar_clustering_node.cpp
@@ -5,15 +5,31 @@ namespace infra_lidar_clustering_node
     using PclPointXYZ = pcl::PointCloud<pcl::PointXYZ>
     InfraLidarClusteringNode::LidaClusteringNode() : nh()
     {
-        point_cloud_sub = nh.ros::Subscriber nh.subscribe(sub_topic, 1, &InfraLidarClusteringNode::PointSub_callback, this);
-            
+        //read parametor from launch file
+        nh.param("lidar_clustering/clip_min_width", clip_min_width, -0.5);
+        nh.param("lidar_clustering/clip_max_width", clip_max_witdh, 0.5);
+        nh.param("lidar_clustering/clip_min_distance", clip_min_distance, 0.2);
+        nh.param("lidar_clustering/clip_max_distance", clip_max_distance, 3.0);
+        nh.param("lidar_clustering/clip_min_height", clip_min_height, -0.85);
+        nh.param("lidar_clustering/clip_max_height", clip_max_height, 0.5);
+        
+        //subscliber by using message_filters
+        pose_sub.subsclibe(nh, "/infra_current_pose", 1);
+        clippedCloud_sub.subsclibe(nh, "/points_no_ground", 1);
+        Sync(SyPl(10), pose_sub, clippedCloud_sub);
+        Sync.registerCallback(boost::bind(&sync_callback, _1, _2));
     }
 
-    void InfraLidarClusteringNode::PointSub_callback(const sensor_msgs::PointCloud2ConstPtr& in_msg)
+    void InfraLidarClusteringNode::sync_callback(const geometry_msgs::PoseStamped::ConstPtr& in_pose_msg,const sensor_msgs::PointCloud2ConstPtr& in_cloud_msg)
     {
-        PclPointXYZ::Ptr current_sub_cloud_ptr(new PclPointXYZ);
-        pcl::fromROSMsg(*in_msg, *current_sub_cloud_ptr);
-        PclPointXYZ::Ptr cliped_cloud_ptr(new PclPointXYZ);
-        clipCloud(current_sub_cloud_ptr, cliped_cloud_ptr, )
+        PclPointXYZ::Ptr current_in_points_ptr(new PclPointXYZ);
+        pcl::fromROSMsg(*in_cloud_msg, *current_in_points);
+        heder_ = in_cloud_msg->header;
+
+        PclPointXYZ::Ptr clipped_cloud(new PclPointXYZ);
+        geometry_msgs::PoseStamped
+        ClippedCloud(current_in_points_ptr, )
+
     }
+
 }
